3521df9d68c6873ff371b2363870825581e34698
Adam Adams <ajadams@gmail.com>
2016-01-26 06:33:19 -0800

    construction of key press log

--- assets/js/react/piano_component.jsx
+++ assets/js/react/piano_component.jsx
@@ -1,70 +1,44 @@
 // React - piano_component.jsx
 
-var B_Key = React.createClass({
+
+
+// ----- Log and log item components
+var LogItem = React.createClass({
 	render: function(){
 		return (
-			<div className="key b-key">
-				B
+			<div className="item">
+				{this.props.text}
 			</div>
 		);
 	}
 });
 
-var A_Key = React.createClass({
+var KeyLog = React.createClass({
 	render: function(){
 		return (
-			<div className="key a-key">
-				A
+			<div className="log">
+				<h3>Key Log</h3>
+				{
+					this.props.keys.length > 0 ?
+					this.props.keys.map(function(val, index, arr){
+						return <LogItem key={index} text={val} />
+					}) :
+					null
+				}
 			</div>
 		);
 	}
 });
+// ----- Log and log item components
 
-var G_Key = React.createClass({
-	render: function(){
-		return (
-			<div className="key g-key">
-				G
-			</div>
-		);
-	}
-});
 
-var F_Key = React.createClass({
-	render: function(){
-		return (
-			<div className="key f-key">
-				F
-			</div>
-		);
-	}
-});
 
-var E_Key = React.createClass({
-	render: function(){
-		return (
-			<div className="key e-key">
-				E
-			</div>
-		);
-	}
-});
-
-var D_Key = React.createClass({
-	render: function(){
-		return (
-			<div className="key d-key">
-				D
-			</div>
-		);
-	}
-});
-
+// ----- Piano and piano key components
 var White_Key = React.createClass({
 	render: function(){
 		return (
-			<div className={this.props.css}>
-				C
+			<div className={this.props.css} onClick={this.props.click}>
+				{this.props.text}
 			</div>
 		);
 	}
@@ -73,27 +47,67 @@ var White_Key = React.createClass({
 var Black_Key = React.createClass({
 	render: function(){
 		return (
-			<div className="black-key">
+			<div className={this.props.css}>
 
 			</div>
 		);
 	}
 });
 
-var Piano_Component = React.createClass({
+var Piano = React.createClass({
 	render: function(){
 		return (
 			<div className="piano">
-				<White_Key css={'key c-key text-center'} />
-				<White_Key css={'key d-key text-center'} />
-				<White_Key css={'key e-key text-center'} />
-				<White_Key css={'key f-key text-center'} />
-				<White_Key css={'key g-key text-center'} />
-				<White_Key css={'key a-key text-center'} />
-				<White_Key css={'key b-key text-center'} />
+				<White_Key css={'key c-key text-center'} text={'C'} click={this.props.click} />
+				<White_Key css={'key d-key text-center'} text={'D'} click={this.props.click} />
+				<White_Key css={'key e-key text-center'} text={'E'} click={this.props.click} />
+				<White_Key css={'key f-key text-center'} text={'F'} click={this.props.click} />
+				<White_Key css={'key g-key text-center'} text={'G'} click={this.props.click} />
+				<White_Key css={'key a-key text-center'} text={'A'} click={this.props.click} />
+				<White_Key css={'key b-key text-center'} text={'B'} click={this.props.click} />
+				<Black_Key css={'black-keys cd-key'} />
+				<Black_Key css={'black-keys de-key'} />
+				<Black_Key css={'black-keys fg-key'} />
+				<Black_Key css={'black-keys ga-key'} />
+				<Black_Key css={'black-keys ab-key'} />
+			</div>
+		);
+	}
+});
+// ----- Piano and piano key components
+
+
+
+// ----- top level app component
+var App_Component = React.createClass({
+	getInitialState: function(){
+		return {
+			keyLog: [],
+		};
+	},
+
+	keyPressed: function(e){
+		this.addToLog( $(e.target).text() );
+	},
+
+	addToLog: function(key){
+		console.log(this.state.keyLog);
+		this.state.keyLog.push(key);
+		this.setState({keyLog: this.state.keyLog});
+	},
+
+	autoplay: function(){
+
+	},
+
+	render: function(){
+		return (
+			<div className="app-container">
+				<Piano click={this.keyPressed} />
+				<KeyLog keys={this.state.keyLog} />
 			</div>
 		);
 	}
 });
 
-ReactDOM.render( <Piano_Component />, document.getElementById('main-container') );
+ReactDOM.render( <App_Component />, document.getElementById('main-container') );
--- public/js/piano_component.js
+++ public/js/piano_component.js
@@ -63,8 +63,8 @@ var D_Key = React.createClass({displayName: "D_Key",
 var White_Key = React.createClass({displayName: "White_Key",
 	render: function(){
 		return (
-			React.createElement("div", {className: this.props.css}, 
-				"C"
+			React.createElement("div", {className: this.props.css, onClick: this.props.click}, 
+				this.props.text
 			)
 		);
 	}
@@ -73,8 +73,39 @@ var White_Key = React.createClass({displayName: "White_Key",
 var Black_Key = React.createClass({displayName: "Black_Key",
 	render: function(){
 		return (
-			React.createElement("div", {className: "black-key"}
+			React.createElement("div", {className: this.props.css}
 
+			)
+		);
+	}
+});
+
+var LogItem = React.createClass({displayName: "LogItem",
+	render: function(){
+		return (
+			React.createElement("div", {className: "item"}, 
+				this.props.text
+			)
+		);
+	}
+});
+
+var KeyLog_Component = React.createClass({displayName: "KeyLog_Component",
+	componentWillMount: function(){
+
+	},
+
+	render: function(){
+		return (
+			React.createElement("div", {className: "log"}, 
+				React.createElement("h3", null, "Key Log"), 
+				
+					this.props.keys.length > 0 ?
+					this.props.keys.map(function(val, index, arr){
+						return React.createElement(LogItem, {key: index, text: val})
+					}) :
+					null
+				
 			)
 		);
 	}
@@ -84,16 +115,52 @@ var Piano_Component = React.createClass({displayName: "Piano_Component",
 	render: function(){
 		return (
 			React.createElement("div", {className: "piano"}, 
-				React.createElement(White_Key, {css: 'key c-key text-center'}), 
-				React.createElement(White_Key, {css: 'key d-key text-center'}), 
-				React.createElement(White_Key, {css: 'key e-key text-center'}), 
-				React.createElement(White_Key, {css: 'key f-key text-center'}), 
-				React.createElement(White_Key, {css: 'key g-key text-center'}), 
-				React.createElement(White_Key, {css: 'key a-key text-center'}), 
-				React.createElement(White_Key, {css: 'key b-key text-center'})
+				React.createElement(White_Key, {css: 'key c-key text-center', text: 'C', click: this.props.click}), 
+				React.createElement(White_Key, {css: 'key d-key text-center', text: 'D', click: this.props.click}), 
+				React.createElement(White_Key, {css: 'key e-key text-center', text: 'E', click: this.props.click}), 
+				React.createElement(White_Key, {css: 'key f-key text-center', text: 'F', click: this.props.click}), 
+				React.createElement(White_Key, {css: 'key g-key text-center', text: 'G', click: this.props.click}), 
+				React.createElement(White_Key, {css: 'key a-key text-center', text: 'A', click: this.props.click}), 
+				React.createElement(White_Key, {css: 'key b-key text-center', text: 'B', click: this.props.click}), 
+				React.createElement(Black_Key, {css: 'black-keys cd-key'}), 
+				React.createElement(Black_Key, {css: 'black-keys de-key'}), 
+				React.createElement(Black_Key, {css: 'black-keys fg-key'}), 
+				React.createElement(Black_Key, {css: 'black-keys ga-key'}), 
+				React.createElement(Black_Key, {css: 'black-keys ab-key'})
 			)
 		);
 	}
 });
 
-ReactDOM.render( React.createElement(Piano_Component, null), document.getElementById('main-container') );
+var App_Component = React.createClass({displayName: "App_Component",
+	getInitialState: function(){
+		return {
+			keyLog: [],
+		};
+	},
+
+	keyPressed: function(e){
+		this.addToLog( $(e.target).text() );
+	},
+
+	addToLog: function(key){
+		console.log(this.state.keyLog);
+		this.state.keyLog.push(key);
+		this.setState({keyLog: this.state.keyLog});
+	},
+
+	autoplay: function(){
+
+	},
+
+	render: function(){
+		return (
+			React.createElement("div", {className: "app-container"}, 
+				React.createElement(Piano_Component, {click: this.keyPressed}), 
+				React.createElement(KeyLog_Component, {keys: this.state.keyLog})
+			)
+		);
+	}
+});
+
+ReactDOM.render( React.createElement(App_Component, null), document.getElementById('main-container') );
