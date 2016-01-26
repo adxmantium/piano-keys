5d2c5ec1818791846303604765b3d2baeae55ca9
Adam Adams <ajadams@gmail.com>
2016-01-25 22:39:09 -0800

    piano key structure

--- assets/js/react/piano_component.jsx
+++ assets/js/react/piano_component.jsx
@@ -1,10 +1,96 @@
 // React - piano_component.jsx
 
+var B_Key = React.createClass({
+	render: function(){
+		return (
+			<div className="key b-key">
+				B
+			</div>
+		);
+	}
+});
+
+var A_Key = React.createClass({
+	render: function(){
+		return (
+			<div className="key a-key">
+				A
+			</div>
+		);
+	}
+});
+
+var G_Key = React.createClass({
+	render: function(){
+		return (
+			<div className="key g-key">
+				G
+			</div>
+		);
+	}
+});
+
+var F_Key = React.createClass({
+	render: function(){
+		return (
+			<div className="key f-key">
+				F
+			</div>
+		);
+	}
+});
+
+var E_Key = React.createClass({
+	render: function(){
+		return (
+			<div className="key e-key">
+				E
+			</div>
+		);
+	}
+});
+
+var D_Key = React.createClass({
+	render: function(){
+		return (
+			<div className="key d-key">
+				D
+			</div>
+		);
+	}
+});
+
+var White_Key = React.createClass({
+	render: function(){
+		return (
+			<div className={this.props.css}>
+				C
+			</div>
+		);
+	}
+});
+
+var Black_Key = React.createClass({
+	render: function(){
+		return (
+			<div className="black-key">
+
+			</div>
+		);
+	}
+});
+
 var Piano_Component = React.createClass({
 	render: function(){
 		return (
-			<div className="top-level-component-container">
-				Testing
+			<div className="piano">
+				<White_Key css={'key c-key text-center'} />
+				<White_Key css={'key d-key text-center'} />
+				<White_Key css={'key e-key text-center'} />
+				<White_Key css={'key f-key text-center'} />
+				<White_Key css={'key g-key text-center'} />
+				<White_Key css={'key a-key text-center'} />
+				<White_Key css={'key b-key text-center'} />
 			</div>
 		);
 	}
--- public/js/piano_component.js
+++ public/js/piano_component.js
@@ -1,10 +1,96 @@
 // React - piano_component.jsx
 
+var B_Key = React.createClass({displayName: "B_Key",
+	render: function(){
+		return (
+			React.createElement("div", {className: "key b-key"}, 
+				"B"
+			)
+		);
+	}
+});
+
+var A_Key = React.createClass({displayName: "A_Key",
+	render: function(){
+		return (
+			React.createElement("div", {className: "key a-key"}, 
+				"A"
+			)
+		);
+	}
+});
+
+var G_Key = React.createClass({displayName: "G_Key",
+	render: function(){
+		return (
+			React.createElement("div", {className: "key g-key"}, 
+				"G"
+			)
+		);
+	}
+});
+
+var F_Key = React.createClass({displayName: "F_Key",
+	render: function(){
+		return (
+			React.createElement("div", {className: "key f-key"}, 
+				"F"
+			)
+		);
+	}
+});
+
+var E_Key = React.createClass({displayName: "E_Key",
+	render: function(){
+		return (
+			React.createElement("div", {className: "key e-key"}, 
+				"E"
+			)
+		);
+	}
+});
+
+var D_Key = React.createClass({displayName: "D_Key",
+	render: function(){
+		return (
+			React.createElement("div", {className: "key d-key"}, 
+				"D"
+			)
+		);
+	}
+});
+
+var White_Key = React.createClass({displayName: "White_Key",
+	render: function(){
+		return (
+			React.createElement("div", {className: this.props.css}, 
+				"C"
+			)
+		);
+	}
+});
+
+var Black_Key = React.createClass({displayName: "Black_Key",
+	render: function(){
+		return (
+			React.createElement("div", {className: "black-key"}
+
+			)
+		);
+	}
+});
+
 var Piano_Component = React.createClass({displayName: "Piano_Component",
 	render: function(){
 		return (
-			React.createElement("div", {className: "top-level-component-container"}, 
-				"Testing"
+			React.createElement("div", {className: "piano"}, 
+				React.createElement(White_Key, {css: 'key c-key text-center'}), 
+				React.createElement(White_Key, {css: 'key d-key text-center'}), 
+				React.createElement(White_Key, {css: 'key e-key text-center'}), 
+				React.createElement(White_Key, {css: 'key f-key text-center'}), 
+				React.createElement(White_Key, {css: 'key g-key text-center'}), 
+				React.createElement(White_Key, {css: 'key a-key text-center'}), 
+				React.createElement(White_Key, {css: 'key b-key text-center'})
 			)
 		);
 	}
